c38d6e43f4916bb6ccc6089f3054ea76fabe1c65
Michael kneuss <micki6491@hotmail.com>
2016-09-27 16:40:59 +0200

    ébauche de l'interface du simulateur (paramètre robot)

--- Projet1He-ARC/RobotSimulator/Robot.cs
+++ Projet1He-ARC/RobotSimulator/Robot.cs
@@ -6,7 +6,145 @@ using System.Threading.Tasks;
 
 namespace RobotSimulator
 {
-    class Robot
+    class Robot : RobotI
     {
+
+        public bool init
+        {
+            get
+            {
+                return init;
+            }
+
+            set
+            {
+                init = value;
+            }
+        }
+
+        public int longueurBrasA
+        {
+            get
+            {
+                return longueurBrasA;
+            }
+
+            set
+            {
+                longueurBrasA = value;
+            }
+        }
+
+        public int longueurBrasB
+        {
+            get
+            {
+                return longueurBrasB;
+            }
+
+            set
+            {
+                longueurBrasB = value;
+            }
+        }
+
+        public bool penActive
+        {
+            get
+            {
+                return penActive;
+            }
+        }
+
+        public string pos
+        {
+            get
+            {
+                return pos;
+            }
+
+            set
+            {
+                pos = value;
+            }
+        }
+
+        public string paramPen
+        {
+            get
+            {
+                return paramPen;
+            }
+
+            set
+            {
+                paramPen = value;
+            }
+        }
+
+        public string paramRobot
+        {
+            get
+            {
+                return paramRobot;
+            }
+
+            set
+            {
+                paramRobot = value;
+            }
+        }
+
+        public int vitesse
+        {
+            get
+            {
+                return vitesse;
+            }
+
+            set
+            {
+                vitesse = value;
+            }
+        }
+
+        public int rotationA
+        {
+            get
+            {
+                return rotationA;
+            }
+
+            set
+            {
+                rotationA = value;
+            }
+        }
+
+        public int rotationB
+        {
+            get
+            {
+                return rotationB;
+            }
+
+            set
+            {
+                rotationB = value;
+            }
+        }
+
+        public Robot(int longueurBrasA, int longueurBrasB, int vitesse, int rotationA, int rotationB)
+        {
+            this.longueurBrasA = longueurBrasA;
+            this.longueurBrasA = longueurBrasB;
+            this.pos = "G1 X-374 Y0<0x0A>";
+            this.paramRobot = "M5 A"+rotationA+" B"+rotationB+" M"+longueurBrasA+" N"+longueurBrasB+" D"+vitesse +"<0x0A>";
+        }
+
+        void RobotI.setParamRobot()
+        {
+            this.paramRobot = "M5 A" + rotationA + " B" + rotationB + " M" + longueurBrasA + " N" + longueurBrasB + " D" + vitesse + "<0x0A>";
+        }
     }
 }
---
+++ Projet1He-ARC/RobotSimulator/robotI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotSimulator
+{
+    interface RobotI
+    {
+        //paramètres du robot
+
+        int longueurBrasA
+        {
+            get;
+            set;
+        }
+
+        int longueurBrasB
+        {
+            get;
+            set;
+        }
+
+        int vitesse
+        {
+            get;
+            set;
+        }
+
+        int rotationA
+        {
+            get;
+            set;
+        }
+
+        int rotationB
+        {
+            get;
+            set;
+        }
+
+        bool init
+        {
+            get;
+            set;
+        }
+
+        string pos
+        {
+            set;
+            get;
+        }
+
+        bool penActive
+        {
+            get;
+        }
+
+        string paramPen
+        {
+            get;
+            set;
+        }
+
+        string paramRobot
+        {
+            get;
+            set;
+        }
+
+        void setParamRobot();
+    }
+}
