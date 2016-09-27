393ba0323fbbd1acd6568998aa4c1471d2525609
Michael Kneuss <michael.kneuss@he-arc.ch>
2016-09-27 12:42:20 +0200

    création du projet + intégration Open CV
    Éléments de travail associés : #3; #17

---
+++ Projet1Groupe4/Projet1Groupe4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV.UI;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Projet1Groupe4
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
